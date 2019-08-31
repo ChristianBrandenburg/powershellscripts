62baa5e4f5d324d394cc5c479cb004e882e87a1c
ChristianBrandenburg <pinconspiracy@gmail.com>
2019-08-31 22:41:47 +0200

    initial script complete

--- opgave2.ps1
+++ opgave2.ps1
@@ -1,34 +1,35 @@
 #!/usr/bin/pwsh
 
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
 #Variabler
 $rg = "opgave2"
 $location = "NorthEurope"
-$pf = "opgave2"
 
 #Resourcegruppe laves
 New-AzResourceGroup -ResourceGroupName $rg -Location $location
 
 #Routing table
 $routeTablePublic = New-AzRouteTable `
-  -Name 'CTBRouteTablePublic' `
+  -Name 'Opg2RouteTablePublic' `
   -ResourceGroupName $rg `
   -location $location
 
 Get-AzRouteTable `
   -ResourceGroupName $rg `
-  -Name $pf + "RouteTablePublic" `
-  Add-AzRouteConfig `
+  -Name "Opg2RouteTablePublic" `
+  | Add-AzRouteConfig `
   -Name "ToPrivateSubnet" `
   -AddressPrefix 10.0.1.0/24 `
   -NextHopType "VirtualAppliance" `
   -NextHopIpAddress 10.0.2.4 `
-  Set-AzRouteTable
+  | Set-AzRouteTable
 
 #Vnet konfigureres
 $virtualNetwork = New-AzVirtualNetwork `
   -ResourceGroupName $rg `
   -Location $location `
-  -Name $pf + "VirtualNetwork" `
+  -Name "Opg2VirtualNetwork" `
   -AddressPrefix 10.0.0.0/16
 
 #subnet laves
@@ -58,7 +59,7 @@ Set-AzVirtualNetwork
 
 # Retrieve the virtual network object into a variable.
 $virtualNetwork=Get-AzVirtualNetwork `
-  -Name $pf + "VirtualNetwork" `
+  -Name "Opg2VirtualNetwork" `
   -ResourceGroupName $rg
 
 # Retrieve the subnet configuration into a variable.
@@ -70,7 +71,7 @@ $subnetConfigDmz = Get-AzVirtualNetworkSubnetConfig `
 $nic = New-AzNetworkInterface `
   -ResourceGroupName $rg `
   -Location $location `
-  -Name $pf +'VmNva' `
+  -Name 'Opg2VmNva' `
   -SubnetId $subnetConfigDmz.Id `
   -EnableIPForwarding
 
@@ -79,10 +80,10 @@ $cred = Get-Credential -Message "Enter a username and password for the VM."
 
 # Create a VM configuration.
 $vmConfig = New-AzVMConfig `
-  -VMName $pf +'VmNva' `
-  -VMSize Standard_DS2 | `
+  -VMName 'Opg2VmNva' `
+  -VMSize "Standard_D1_v2" | `
   Set-AzVMOperatingSystem -Windows `
-    -ComputerName $pf + 'VmNva' `
+    -ComputerName 'Opg2VmNva' `
     -Credential $cred | `
   Set-AzVMSourceImage `
     -PublisherName MicrosoftWindowsServer `
@@ -99,15 +100,15 @@ New-AzVM `
 New-AzVm `
   -ResourceGroupName $rg `
   -Location $location `
-  -VirtualNetworkName $pf +"VirtualNetwork" `
+  -VirtualNetworkName "Opg2VirtualNetwork" `
   -SubnetName "Public" `
   -ImageName "Win2016Datacenter" `
-  -Name $pf + "VmPublic"
+  -Name "Opg2VmPublic"
 
 New-AzVm `
   -ResourceGroupName $rg `
   -Location $location `
-  -VirtualNetworkName $pf + "VirtualNetwork" `
+  -VirtualNetworkName "Opg2VirtualNetwork" `
   -SubnetName "Private" `
   -ImageName "Win2016Datacenter" `
-  -Name $pf + "VmPrivate"
+  -Name "Opg2VmPrivate"
